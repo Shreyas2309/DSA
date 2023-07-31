eb52e51da19726e1e92ff8b555d13193b7593d82
Shreyas Gopnarayan <gopnarayanshreyas2004@gmail.com>
2023-07-31 12:23:30 +0530

    solved till pattern_8

---
+++ 1. Patterns/pattern8.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+    for(int i=0; i<n; i++) {
+        //space
+        for(int j=0; j<i; j++) {
+            cout<<" ";
+        }
+
+        //stars
+        for(int j=0; j<2*n -(2*i +1); j++) {
+            cout<<"*";
+        }
+
+        //space
+        for(int j=0; j<i; j++) {
+            cout<<" ";
+        }
+
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
